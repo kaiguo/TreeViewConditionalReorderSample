aa956a82e3d9a7b02fd1ea84b2ab019f433e7727
Kai Guo <kaigu@microsoft.com>
2019-08-25 21:10:13 -0700

    Test InsertionPanel

--- TreeViewConditionalReorderSample/MainPage.xaml.cs
+++ TreeViewConditionalReorderSample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Controls;
 
@@ -8,10 +9,12 @@ namespace TreeViewConditionalReorderSample
     {
 
         public ObservableCollection<TreeViewData> DataSource { get; set; }
+        public static List<TreeViewData> DraggedItems { get; set; }
 
         public MainPage()
         {
             this.InitializeComponent();
+            DraggedItems = new List<TreeViewData>();
             InitData();
         }
 
@@ -23,6 +26,19 @@ namespace TreeViewConditionalReorderSample
             var item3 = new TreeViewData() { Content = "Leaf 2", IsGroup = false };
             var item4 = new TreeViewData() { Content = "Group 3", IsGroup = true };
             DataSource = new ObservableCollection<TreeViewData> { item1, item2, item3, item4 };
+        }
+
+        private void TreeView_DragItemsStarting(Microsoft.UI.Xaml.Controls.TreeView sender, Microsoft.UI.Xaml.Controls.TreeViewDragItemsStartingEventArgs args)
+        {
+            foreach (var item in args.Items)
+            {
+                DraggedItems.Add(item as TreeViewData);
+            }
+        }
+
+        private void TreeView_DragItemsCompleted(Microsoft.UI.Xaml.Controls.TreeView sender, Microsoft.UI.Xaml.Controls.TreeViewDragItemsCompletedEventArgs args)
+        {
+            DraggedItems.Clear();
         }
 
         private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
--- TreeViewConditionalReorderSample/MyTreeViewItem.cs
+++ TreeViewConditionalReorderSample/MyTreeViewItem.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using DataPackageOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation;
+using MUXC = Microsoft.UI.Xaml.Controls;
 
 namespace TreeViewConditionalReorderSample
 {
-    class MyTreeViewItem : TreeViewItem
+    class MyTreeViewItem : MUXC.TreeViewItem
     {
         protected override void OnDragOver(DragEventArgs e)
         {
@@ -16,10 +19,39 @@ namespace TreeViewConditionalReorderSample
 
         protected override void OnDrop(DragEventArgs e)
         {
-            var data = DataContext as TreeViewData;
-            e.AcceptedOperation = data.IsGroup ? DataPackageOperation.Move : DataPackageOperation.None;
+            DependencyObject parent = this;
+            while(!(parent is MUXC.TreeViewList))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
 
+            var treeViewList = parent as MUXC.TreeViewList;
+            var insertionPanel = treeViewList.ItemsPanelRoot as IInsertionPanel;
+
+            int aboveIndex = -1;
+            int belowIndex = -1;
+            var point = e.GetPosition(insertionPanel as UIElement);
+            insertionPanel.GetInsertionIndexes(point, out aboveIndex, out belowIndex);
+            var treeViewDataAbove = GetDataFromIndex(treeViewList, aboveIndex);
+            var treeViewDataBelow = GetDataFromIndex(treeViewList, belowIndex);
+
+            var data = DataContext as TreeViewData;
+            e.AcceptedOperation = /*IsAbleToDrop*/ ? DataPackageOperation.Move : DataPackageOperation.None;
+
+            var item = MainPage.DraggedItems;
             base.OnDrop(e);
+        }
+
+        private TreeViewData GetDataFromIndex(MUXC.TreeViewList list, int index)
+        {
+            var treeViewItem = list.ContainerFromIndex(index);
+            if(treeViewItem != null)
+            {
+                var data = list.ItemFromContainer(treeViewItem);
+                return data as TreeViewData;
+            }
+
+            return null;
         }
     }
 }
