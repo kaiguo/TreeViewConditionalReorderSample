dc6fc70e4759d694b43eadae74ed0e012788bb78
Kai Guo <kaigu@microsoft.com>
2019-11-20 14:09:54 -0800

    cleaups

--- TreeViewConditionalReorderSample/MainPage.xaml.cs
+++ TreeViewConditionalReorderSample/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Controls;
 
+using MUXC = Microsoft.UI.Xaml.Controls;
+
 namespace TreeViewConditionalReorderSample
 {
     public sealed partial class MainPage : Page
@@ -20,29 +22,30 @@ namespace TreeViewConditionalReorderSample
         {
             DraggedItems = new List<TreeViewData>();
             var item1 = new TreeViewData() { Content = "Group 1", IsGroup = true };
-            item1.Children.Add(new TreeViewData() { Content = "Leaf 1.1", IsGroup = false, HasParent = true });
-            item1.Children.Add(new TreeViewData() { Content = "Leaf 1.2", IsGroup = false, HasParent = true });
+            item1.Children.Add(new TreeViewData() { Content = "Leaf 1.1", IsGroup = false, Parent = item1 });
+            item1.Children.Add(new TreeViewData() { Content = "Leaf 1.2", IsGroup = false, Parent = item1 });
             var item2 = new TreeViewData() { Content = "Leaf 1", IsGroup = false };
             var item3 = new TreeViewData() { Content = "Leaf 2", IsGroup = false };
             var item4 = new TreeViewData() { Content = "Leaf 3", IsGroup = false };
-            var item5 = new TreeViewData() { Content = "Group 3", IsGroup = true };
+            var item5 = new TreeViewData() { Content = "Group 2", IsGroup = true };
             DataSource = new ObservableCollection<TreeViewData> { item1, item2, item3, item4, item5 };
         }
 
-        private void TreeView_DragItemsStarting(Microsoft.UI.Xaml.Controls.TreeView sender, Microsoft.UI.Xaml.Controls.TreeViewDragItemsStartingEventArgs args)
+        private void TreeView_DragItemsStarting(MUXC.TreeView sender, MUXC.TreeViewDragItemsStartingEventArgs args)
         {
-            foreach (var item in args.Items)
+            foreach (TreeViewData item in args.Items)
             {
-                DraggedItems.Add(item as TreeViewData);
+                DraggedItems.Add(item);
             }
         }
 
-        private void TreeView_DragItemsCompleted(Microsoft.UI.Xaml.Controls.TreeView sender, Microsoft.UI.Xaml.Controls.TreeViewDragItemsCompletedEventArgs args)
+        private void TreeView_DragItemsCompleted(MUXC.TreeView sender, MUXC.TreeViewDragItemsCompletedEventArgs args)
         {
             foreach(TreeViewData item in args.Items)
             {
-                item.HasParent = args.NewParent!=null ? true : false;
+                item.Parent = args.NewParent as TreeViewData;
             }
+
             DraggedItems.Clear();
         }
     }
--- TreeViewConditionalReorderSample/MyTreeViewItem.cs
+++ TreeViewConditionalReorderSample/MyTreeViewItem.cs
@@ -6,12 +6,30 @@ namespace TreeViewConditionalReorderSample
 {
     class MyTreeViewItem : TreeViewItem
     {
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            var draggedItem = MainPage.DraggedItems[0];
+            var draggedOverItem = DataContext as TreeViewData;
+            // Block TreeViewNode auto expanding if we are dragging a group onto another group
+            if (draggedItem.IsGroup && draggedOverItem.IsGroup)
+            {
+                e.Handled = true;
+            }
+
+            base.OnDragEnter(e);
+        }
+
         protected override void OnDragOver(DragEventArgs e)
         {
             var draggedItem = MainPage.DraggedItems[0];
             var draggedOverItem = DataContext as TreeViewData;
-            if (draggedItem.IsGroup && (draggedOverItem.IsGroup || draggedOverItem.HasParent))
+
+            if (draggedItem.IsGroup && (draggedOverItem.IsGroup || draggedOverItem.Parent!=null))
             {
+                //- Group
+                //-- Leaf1
+                //-- (Group2) <- Blocks dropping another Group here
+                //-- Leaf2
                 e.Handled = true;
             }
             base.OnDragOver(e);
@@ -20,6 +38,7 @@ namespace TreeViewConditionalReorderSample
         protected override void OnDrop(DragEventArgs e)
         {
             var data = DataContext as TreeViewData;
+            // Block all drops on leaf node
             if(!data.IsGroup)
             {
                 e.Handled = true;
